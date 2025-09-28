9d82087fd6b96b907c4056a792349a257363a18d
mirkopresa <mirkocamilopresa@proton.me>
2025-09-28 19:00:26 -0300

    Funciones

--- Ejercicios/Guia 2/ejercicio_17.c
+++ Ejercicios/Guia 2/ejercicio_17.c
@@ -8,7 +8,7 @@ double producto_wallis(int numero)
     double aproximacion_pi = 1.0;
     int x;
     for (x=1; x <= numero; x++)
-        aproximacion_pi *= (((2.0 * x) / (2.0 * x - 1.0)) * ((2.0 * x) / (2.0 * x + 1.0)));
+        aproximacion_pi *= ((2.0 * x) / (2.0 * x - 1.0)) * ((2.0 * x) / (2.0 * x + 1.0));
     aproximacion_pi *= 2;
     return aproximacion_pi;
 }
